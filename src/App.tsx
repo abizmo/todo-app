96f48aef67dd045ecd19cfa3b4c4c82bb8c4b211
Abián Izquierdo <abizmo@gmail.com>
2022-08-25 22:39:13 +0100

    Add html structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,123 @@
 import React from 'react';
 
+import iconCross from './assets/images/icon-cross.svg';
+import iconMoon from './assets/images/icon-moon.svg';
+import iconSun from './assets/images/icon-sun.svg';
+
 import './App.css';
 
 const App = () => (
   <>
-    <header>
+    <header className='flex app-header'>
       <h1>Todo</h1>
-      <div>theme button</div>
+      <button className='theme-toggle'>
+        <img className='' src={iconSun} alt='' aria-hidden='true' />
+        <img className='' src={iconMoon} alt='' aria-hidden='true' />
+        <span className='visually-hidden'>Change theme</span>
+      </button>
     </header>
     <main>
-      <p>Create a new todo</p>
-      <ul>
-        <li>First task</li>
-        <li>Second task</li>
-        <li>Last task</li>
-      </ul>
-      <p>5 items left</p>
-      <p>All</p>
-      <p>Active</p>
-      <p>Completed</p>
-      <p>Clear Completed</p>
+      <form action=''>
+        <label>
+          <input type='checkbox' />
+          <span className='checkmark'></span>
+          <input type='text' placeholder='Create a new todo...' />
+        </label>
+      </form>
+      <div>
+        <ul className='todo-items' role='list'>
+          <li>
+            <label>
+              <input type='checkbox' />
+              <span className='checkmark'></span>
+              <span>Complete online JavaScript course</span>
+              <button>
+                <img src={iconCross} alt='Delete task' />
+              </button>
+            </label>
+          </li>
+          <li>
+            <label>
+              <input type='checkbox' />
+              <span className='checkmark'></span>
+              <span>Jog aroung the park 3x</span>
+              <button>
+                <img src={iconCross} alt='Delete task' />
+              </button>
+            </label>
+          </li>
+          <li>
+            <label>
+              <input type='checkbox' />
+              <span className='checkmark'></span>
+              <span>10 minutes meditation</span>
+              <button>
+                <img src={iconCross} alt='Delete task' />
+              </button>
+            </label>
+          </li>
+          <li>
+            <label>
+              <input type='checkbox' />
+              <span className='checkmark'></span>
+              <span>Read for 1 hour</span>
+              <button>
+                <img src={iconCross} alt='Delete task' />
+              </button>
+            </label>
+          </li>
+          <li>
+            <label>
+              <input type='checkbox' />
+              <span className='checkmark'></span>
+              <span>Pick up groceries</span>
+              <button>
+                <img src={iconCross} alt='Delete task' />
+              </button>
+            </label>
+          </li>
+          <li>
+            <label>
+              <input type='checkbox' />
+              <span className='checkmark'></span>
+              <span>Complete Todo App on Frontend Mentor</span>
+              <button>
+                <img src={iconCross} alt='Delete task' />
+              </button>
+            </label>
+          </li>
+        </ul>
+        <div className='flex'>
+          <span>5 items left</span>
+          <div className='filter-list' role='tablist' aria-label='filter'>
+            <button
+              aria-selected='true'
+              role='tab'
+              aria-controls='todo-items'
+              tabIndex={0}
+            >
+              All
+            </button>
+            <button
+              aria-selected='true'
+              role='tab'
+              aria-controls='todo-items'
+              tabIndex={-1}
+            >
+              Active
+            </button>
+            <button
+              aria-selected='true'
+              role='tab'
+              aria-controls='todo-items'
+              tabIndex={-1}
+            >
+              Completed
+            </button>
+          </div>
+          <span>Clear Completed</span>
+        </div>
+      </div>
       <p>Drag and drop to reorder list</p>
     </main>
     <footer className='attribution'>
