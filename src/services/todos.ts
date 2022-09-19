ff3ab1a848543d9c62bad1af6a985db6b5880c6e
Abián Izquierdo Montesdeoca <abizmo@gmail.com>
2022-09-19 19:33:07 +0100

    Update todos.ts

--- src/services/todos.ts
+++ src/services/todos.ts
@@ -1,7 +1,7 @@
 import { todos } from '@/fixtures';
 import { TodosType } from '@/models';
 
-const DELAY_MS = 5000;
+const DELAY_MS = 300;
 
 export const getTodos = async (): Promise<TodosType[]> => {
   return new Promise((resolve) => {
